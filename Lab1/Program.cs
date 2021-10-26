b4f074c68ec573d9d6886c397203a1178208ac8f
Ippolitov2909 <vovipp2000@gmail.com>
2021-10-26 03:09:01 +0300

    First task

---
+++ Lab1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using RecognitionLibrary;
+
+namespace Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ObjectDetection.Detect(args[0]);
+        }
+    }
+}
