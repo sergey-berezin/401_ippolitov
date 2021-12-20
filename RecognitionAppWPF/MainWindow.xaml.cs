5c25cf73966780ac6272e0f6d81304316a7dacdd
Ippolitov2909 <vovipp2000@gmail.com>
2021-12-20 15:58:22 +0300

    Assignment 3

--- RecognitionAppWPF/MainWindow.xaml.cs
+++ RecognitionAppWPF/MainWindow.xaml.cs
@@ -32,8 +32,12 @@ namespace RecognitionAppWPF
         private CancellationTokenSource cts = new CancellationTokenSource();
         private CancellationToken token;
         public ImmutableList<BitmapImage> images;
+        public List<DetectedObject> objects;
+        public List<BitmapImage> startImages;
         public string[] filenames;
         public string directoryPath;
+        public DatabaseManager db;
+
 
         public MainWindow()
         {
@@ -42,8 +46,23 @@ namespace RecognitionAppWPF
             directoryPath = "";
             listBox_Images.ItemsSource = new List<BitmapImage>();
             images = ImmutableList.Create<BitmapImage>();
+            startImages = new List<BitmapImage>();
+            objects = new List<DetectedObject>();
+            listBox_Objects.ItemsSource = objects;
+            db = new DatabaseManager();
+
+            ShowDBObjects();
         }
 
+        private void ShowDBObjects()
+        {
+            objects = new List<DetectedObject>();
+            foreach (Image img in db.Images)
+            {
+                objects.AddRange(img.DetectedObjects);
+            }
+            listBox_Objects.ItemsSource = objects;
+        }
 
         private void Open_Click(object sender, RoutedEventArgs e)
         {
@@ -59,16 +78,19 @@ namespace RecognitionAppWPF
                     filenames = Directory.GetFiles(directoryPath);
                     listBox_Images.ItemsSource = new List<BitmapImage>();
                     images = ImmutableList.Create<BitmapImage>();
-                    foreach (var filename in filenames) {
+                    foreach (var filename in filenames)
+                    {
                         try
                         {
                             images = images.Add(new BitmapImage(new Uri(filename)));
-                        } catch
+                        }
+                        catch
                         {
                             continue;
                         }
                     }
                     listBox_Images.ItemsSource = images;
+                    startImages = images.ToList();
                 }
             }
             catch (Exception exception)
@@ -82,7 +104,7 @@ namespace RecognitionAppWPF
         {
             cts.Cancel();
         }
-        
+
         private BitmapImage BitmapImageFromBitmap(Bitmap bitmap)
         {
             using (var memory = new MemoryStream())
@@ -112,11 +134,20 @@ namespace RecognitionAppWPF
                 return new Bitmap(bitmap);
             }
         }
+        private byte[] BitmapToByteArray(Bitmap bitmap)
+        {
+            using (var memoryStream = new MemoryStream())
+            {
+                bitmap.Save(memoryStream, System.Drawing.Imaging.ImageFormat.Png);
+                return memoryStream.ToArray();
+            }
+        }
 
         private async void Start_Click(object sender, RoutedEventArgs e)
         {
             Open.IsEnabled = false;
             Start.IsEnabled = false;
+            Clear.IsEnabled = false;
             var detectionResults = new ConcurrentQueue<Tuple<string, YoloV4Result>>();
             cts = new CancellationTokenSource();
             token = cts.Token;
@@ -130,44 +161,49 @@ namespace RecognitionAppWPF
 
                 var writeResultsTask = Task.Factory.StartNew(() =>
                 {
-                this.Dispatcher.BeginInvoke(new Action(() =>
-                {
-                    Status.Text = "Detection started";
-                }));
-                while (recognitionTask.Status == TaskStatus.Running)
-                {
-                    while (detectionResults.TryDequeue(out Tuple<string, YoloV4Result> result))
+                    this.Dispatcher.BeginInvoke(new Action(() =>
                     {
-                        var filename = result.Item1;
-                        int index = Array.FindIndex(filenames, val => val == filename);
-                        if (index < 0)
+                        Status.Text = "Detection started";
+                    }));
+                    while (recognitionTask.Status == TaskStatus.Running)
+                    {
+                        while (detectionResults.TryDequeue(out Tuple<string, YoloV4Result> result))
                         {
-                            continue;
-                        }
-                        var bitmap = BitmapFromBitmapImage(images[index]);
-                        var g = Graphics.FromImage(bitmap);
+                            var filename = result.Item1;
+                            int index = Array.FindIndex(filenames, val => val == filename);
+                            if (index < 0)
+                            {
+                                continue;
+                            }
+                            var bitmap = BitmapFromBitmapImage(images[index]);
+                            var g = Graphics.FromImage(bitmap);
 
-                        var detectedObject = result.Item2;
-                        var x1 = detectedObject.BBox[0];
-                        var y1 = detectedObject.BBox[1];
-                        var x2 = detectedObject.BBox[2];
-                        var y2 = detectedObject.BBox[3];
+                            var detectedObject = result.Item2;
+                            var x1 = detectedObject.BBox[0];
+                            var y1 = detectedObject.BBox[1];
+                            var x2 = detectedObject.BBox[2];
+                            var y2 = detectedObject.BBox[3];
 
-                        g.DrawRectangle(Pens.Red, x1, y1, x2 - x1, y2 - y1);
-                        var brushes = new SolidBrush(Color.FromArgb(30, Color.Red));
-                            
-                        g.FillRectangle(brushes, x1, y1, x2 - x1, y2 - y1);
-                        g.DrawString(detectedObject.Label + ',' + detectedObject.Confidence.ToString(),
-                            new Font("Arial", 16), Brushes.Indigo, new PointF(x1, y1));
+                            g.DrawRectangle(Pens.Red, x1, y1, x2 - x1, y2 - y1);
+                            var brushes = new SolidBrush(Color.FromArgb(30, Color.Red));
+
+                            g.FillRectangle(brushes, x1, y1, x2 - x1, y2 - y1);
+                            g.DrawString(detectedObject.Label + ',' + detectedObject.Confidence.ToString(),
+                                new Font("Arial", 16), Brushes.Indigo, new PointF(x1, y1));
 
 
-                        images = images.RemoveAt(index);
-                        images = images.Insert(index, BitmapImageFromBitmap(bitmap));
-                        this.Dispatcher.BeginInvoke(new Action(() =>
-                        {
-                            listBox_Images.ItemsSource = images;
-                            Status.Text = "Detected: " + detectedObject.Label;
-                        }));
+                            images = images.RemoveAt(index);
+                            images = images.Insert(index, BitmapImageFromBitmap(bitmap));
+                            this.Dispatcher.BeginInvoke(new Action(() =>
+                            {
+                                listBox_Images.ItemsSource = images;
+                                Status.Text = "Detected: " + detectedObject.Label;
+                            }));
+
+                            byte[] bytes = BitmapToByteArray(BitmapFromBitmapImage(startImages[index]));
+                            DetectedObject detected = new DetectedObject { ClassName = detectedObject.Label, Top = x1, Bottom = x2, Left = y1, Right = y2 };
+                            db.AddObject(new Image { Content = bytes, ImageHash = DatabaseManager.CountHash(bytes) }, detected);
+                            db.SaveChanges();
                         }
                     }
                     this.Dispatcher.BeginInvoke(new Action(() =>
@@ -179,13 +215,30 @@ namespace RecognitionAppWPF
             }
             catch (Exception ex)
             {
-                Status.Text = ex.Message; 
+                Status.Text = ex.Message;
             }
             finally
             {
                 Open.IsEnabled = true;
                 Start.IsEnabled = true;
+                Clear.IsEnabled = true;
+                ShowDBObjects();
             }
+        }
+        private void Clear_Click(object sender, RoutedEventArgs e)
+        {
+            var query = db.Images;
+            foreach (Image image in query)
+            {
+                db.Images.Remove(image);
+            }
+            var query2 = db.Objects;
+            foreach (DetectedObject detected in query2)
+            {
+                db.Objects.Remove(detected);
+            }
+            db.SaveChanges();
+            ShowDBObjects();
         }
     }
 }
